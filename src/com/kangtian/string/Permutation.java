4833b6d218f0dcb6eb3a66aa1162b70bb9b70ba2
kangtian.poon <kangtian.poon@vipshop.com>
2018-09-03 20:53:01 +0800

    ...

--- src/com/kangtian/string/Permutation.java
+++ src/com/kangtian/string/Permutation.java
@@ -5,6 +5,30 @@ import java.util.ArrayList;
 public class Permutation {
 
     public ArrayList<String> Permutation(String str) {
+    str=removeRepeatChar(str);
+        String[] arr = str.split(str);
+     for ()
+
     return null;
     }
-}
+
+
+
+    public String removeRepeatChar(String s) {
+        if (s == null) {
+            return "";
+        }
+        StringBuffer sb = new StringBuffer();
+        int i = 0;
+        int len = s.length();
+        while (i < len) {
+            char c = s.charAt(i);
+            sb.append(c);
+            i++;
+            while (i < len && s.charAt(i) == c) {//这个是如果这两个值相等，就让i+1取下一个元素
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+    }
